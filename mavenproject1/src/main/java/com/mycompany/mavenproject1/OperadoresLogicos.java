2cf8776cd63002cee7058947f54736ae4297cc3b
Danilo-Miguel <dan.sm.mineiro@gmail.com>
2024-03-11 11:14:43 -0300

    Inicio do repositório

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/OperadoresLogicos.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Samsung
+ */public class OperadoresLogicos {
+    public static void main(String[] args) {
+        
+        int a = 17;
+        int b  = 17;
+        boolean conE =  a >= b && a == b;
+        System.out.println("Valor " + conE);
+        int d = 14;
+        int e  = 17;
+        
+        boolean conOuN =  !(d < e || d == e);
+
+        boolean conOu =  (d < e || d == e);
+        System.out.println("Valor " + conOu);
+                System.out.println("Valor " + conOuN);
+
+    }
+    
+}
