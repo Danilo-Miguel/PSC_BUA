6234ef442df45c442292c5396f3aefc1087a8b5c
Danilo-Miguel <dan.sm.mineiro@gmail.com>
2024-03-25 09:23:23 -0300

    Entrada e saida JOptionPane

--- mavenproject1/src/main/java/entradaSaida/NumeroInteiro.java
+++ mavenproject1/src/main/java/entradaSaida/NumeroInteiro.java
@@ -4,10 +4,20 @@
  */
 package entradaSaida;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Samsung
  */
 public class NumeroInteiro {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+
+        System.out.print("Digite um número inteiro: ");
+        numero = scanner.nextInt();
+
+        System.out.println("O número digitado foi: " + numero);
+    }
 }
